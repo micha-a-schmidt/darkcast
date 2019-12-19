3c75aa2cd5313b8e3b9ae2493ad907d65f3dff47
philten <philten@cern.ch>
2019-12-19 17:12:02 +0000

    Added new NA64 limits.

--- limits/NA64_NA642019imj.py
+++ limits/NA64_NA642019imj.py
@@ -2,8 +2,8 @@
 # Copyright (C) 2019 Philip Ilten, Yotam Soreq, Mike Williams, and Wei Xue.
 import darkcast
 notes = """
-This limit was extracted from figure 15 (blue curve labeled NA64) of
-Banerjee:2017hhz.
+This limit was extracted from figure 3 (blue curve labeled NA64) of
+NA64:2019imj.
 
 This search is an invisible search and so the efficiency ratio is
 assumed to be unity, e.g. t0 = 0 and t1 = infinity. Consequently, the
@@ -12,26 +12,25 @@ visible width. Here, the invisible width is set as 99 times the
 visible width, e.g. a branching fraction of 99%.
 """
 bibtex = """
-@article{Banerjee:2017hhz,
+@article{NA64:2019imj,
  author         = "Banerjee, D. and others",
- title          = "{Search for vector mediator of Dark Matter production in
-                   invisible decay mode}",
- collaboration  = "NA64",
- journal        = "Phys. Rev.",
- volume         = "D97",
- year           = "2018",
- number         = "7",
- pages          = "072002",
- doi            = "10.1103/PhysRevD.97.072002",
- eprint         = "1710.00971",
+ title          = "{Dark matter search in missing energy events with NA64}",
+ journal        = "Phys. Rev. Lett.",
+ volume         = "123",
+ year           = "2019",
+ number         = "12",
+ pages          = "121801",
+ doi            = "10.1103/PhysRevLett.123.121801",
+ eprint         = "1906.00176",
  archivePrefix  = "arXiv",
  primaryClass   = "hep-ex",
- SLACcitation   = "%%CITATION = ARXIV:1710.00971;%%"
+ reportNumber   = "CERN-EP-2019-116",
+ SLACcitation   = "%%CITATION = ARXIV:1906.00176;%%"
 }
 """
 model = darkcast.Model("dark_photon", iwidth = lambda m, model:
                        99.0*model.width("visible", m))
 production = darkcast.Production("e_brem")
 decay = "invisible"
-bounds = darkcast.Dataset("limits/NA64_Banerjee2017hhz.lmt")
+bounds = darkcast.Dataset("limits/NA64_NA642019imj.lmt")
 efficiency = darkcast.Efficiency(t0 = 0, t1 = float("inf"))
