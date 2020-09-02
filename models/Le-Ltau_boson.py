d7ad5401294e3348191518eef833a2410eec5cec
philten <philten@cern.ch>
2020-09-02 17:48:45 +0100

    Added Le-Ltau.

---
+++ models/Le-Ltau_boson.py
@@ -0,0 +1,18 @@
+# DARKCAST is licensed under the GNU GPL version 2 or later.
+# Copyright (C) 2020 Philip Ilten, Yotam Soreq, Mike Williams, and Wei Xue.
+
+# Define the fermion couplings.
+xfs = {
+    'e':      1,
+    'mu':     0,
+    'tau':   -1,
+    'nue':    1,
+    'numu':   0,
+    'nutau': -1,
+    'd':      0,
+    'u':      0,
+    's':      0,
+    'c':      0,
+    'b':      0,
+    't':      0
+    }
