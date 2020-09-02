45b794f9cf37799a824e2bcb1de12d1baf11b667
philten <philten@cern.ch>
2020-09-02 17:13:31 +0100

    Added lepton number models.

---
+++ models/Le-Lmu_boson.py
@@ -0,0 +1,18 @@
+# DARKCAST is licensed under the GNU GPL version 2 or later.
+# Copyright (C) 2020 Philip Ilten, Yotam Soreq, Mike Williams, and Wei Xue.
+
+# Define the fermion couplings.
+xfs = {
+    'e':      1,
+    'mu':    -1,
+    'tau':    0,
+    'nue':    1,
+    'numu':  -1,
+    'nutau':  0,
+    'd':      0,
+    'u':      0,
+    's':      0,
+    'c':      0,
+    'b':      0,
+    't':      0
+    }
